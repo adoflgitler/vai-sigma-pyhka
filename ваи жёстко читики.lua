48211e0b3a9e4a9979490cb057ad662ead429eb1
adoflgitler <kola.syrn.9@gmail.com>
2025-02-26 19:47:43 +0500

    ваи жёстко читики.lua

--- ваи жёстко читики.lua
+++ ваи жёстко читики.lua
@@ -1,40 +1,24 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Events = ReplicatedStorage:WaitForChild("Events")
-local Functions = ReplicatedStorage:WaitForChild("Functions")
-local VoteForMap = Events:WaitForChild("VoteForMap")
-local VoteSkip = Functions:WaitForChild("VoteSkip")
-local SpawnTower = Functions:WaitForChild("SpawnTower")
-local ReplayButtonPressed = ReplicatedStorage:WaitForChild("ReplayButtonPressed")
+local VoteForMapEvent = ReplicatedStorage:WaitForChild("Events"):WaitForChild("VoteForMap")
+local VoteSkipFunction = ReplicatedStorage:WaitForChild("Functions"):WaitForChild("VoteSkip")
+local ChangeSpeedEvent = ReplicatedStorage:WaitForChild("Events"):WaitForChild("ChangeSpeed")
+local ReplayButtonPressedEvent = ReplicatedStorage:WaitForChild("ReplayButtonPressed")
 
-VoteForMap:FireServer("FNAF 9")
-wait(9)
+local args = { [1] = "Level 5" }
+VoteForMapEvent:FireServer(unpack(args))
+wait(11)
 
-spawn(function()
-    while true do
-        VoteSkip:InvokeServer()
-        wait(0)
-    end
-end)
-
-wait(51)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.375031471252441, 23.6176815032959, 830.199462890625)
-local function spawnTower(towerName, cframe)
-    SpawnTower:InvokeServer(towerName, cframe)
+local function voteSkipLoop()
+  while task.wait() do
+    VoteSkipFunction:InvokeServer()
+  end
 end
 
-local function spawn(towerName, cframe)
-    SpawnTower:InvokeServer(towerName, cframe)
-end
+coroutine.wrap(voteSkipLoop)()
 
-spawn("Helpi", CFrame.new(-4.496251583099365, 21.694805145263672, 439.75115966796875, 1, 2.476920180072284e-08, -3.3321800874119845e-10, -2.476920180072284e-08, 1, -1.2382261015131402e-13, 3.3321800874119845e-10, 1.2383086364035206e-13, 1))
-wait(14)
-spawn("Helpi", CFrame.new(-4.496251583099365, 21.694805145263672, 439.75115966796875, 1, 2.476920180072284e-08, -3.3321800874119845e-10, -2.476920180072284e-08, 1, -1.2382261015131402e-13, 3.3321800874119845e-10, 1.2383086364035206e-13, 1))
-wait(15)
-spawn("Helpi", CFrame.new(-4.496251583099365, 21.694805145263672, 439.75115966796875, 1, 2.476920180072284e-08, -3.3321800874119845e-10, -2.476920180072284e-08, 1, -1.2382261015131402e-13, 3.3321800874119845e-10, 1.2383086364035206e-13, 1))
-wait(15)
-spawn("Front Man", CFrame.new(6.597783088684082, 21.993024826049805, 503.9781799316406, 1, 0, 0, 0, 1, 0, 0, 0, 1))
-wait(7)
-spawn("Front Man", CFrame.new(22.24449348449707, 21.993024826049805, 544.4930419921875, 1, 0, 0, 0, 1, 2.0194839173657902e-28, -2.758576516270559e-36, 6.310887241768094e-29, 1))
+wait(0) 
 
-wait(38)
-ReplayButtonPressed:FireServer()
+local args2 = { [1] = "inf" }
+ChangeSpeedEvent:FireServer(unpack(args2))
+wait(35)
+ReplayButtonPressedEvent:FireServer()
